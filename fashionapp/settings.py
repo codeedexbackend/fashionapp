58e400b9d02bbcc82f8757bc79496a6c6cc5b807
codeedexbackend <prithew.codeedex@gmail.com>
2024-04-12 20:42:05 +0530

    web

--- fashionapp/settings.py
+++ fashionapp/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'web',
     'rest_framework',
     'django_otp',
     'django_filters',
--- fashionapp/urls.py
+++ fashionapp/urls.py
@@ -7,5 +7,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('api.urls')),
+    path('',include('web.urls')),
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
